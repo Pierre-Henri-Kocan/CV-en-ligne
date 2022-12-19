4f050a3cd0b72a41aae7ab0eeb67d28c8c4cd47d
Pierre-Henri Kocan <pierre-henrikocan@macbook-pro-de-pierre-henri.home>
2022-12-19 16:25:39 +0100

    home, experience, formation and technology templates created

---
+++ php/inc/templates/experience.tpl.php
@@ -0,0 +1,43 @@
+<div class="timeline">
+  <div class="outer">
+    <div class="card-experience">
+      <div class="info">
+          <h3 class="title">Projet de fin de formation</h3>
+          <h4 class="card-experience-date">Août 2022</h4>
+          <h5 class="card-experience-location">Ecole O’clock – Téléprésentiel</h5>
+          <p class="card-experience-detail">Rédaction d’un cahier des charges</p>
+          <p class="card-experience-detail">Création de wireframes et user stories</p>
+          <p class="card-experience-detail">Travail collaboratif selon la méthode AGILE SCRUM</p>
+          <p class="card-experience-detail">Développement de fonctionnalités</p>
+          <p class="card-experience-detail">Versionning avec Git/GitHub</p>
+      </div>
+    </div>
+    <div class="card-experience">
+      <div class="info">
+        <h3 class="title">Responsable flotte</h3>
+        <h4 class="card-experience-date">2017-2022</h4>
+        <h5 class="card-experience-location">ALD automotive - Nanterre (92)</h5>
+        <p class="card-experience-detail">Gérer et optimiser la flotte moyenne durée.</p>
+        <p class="card-experience-detail">Manager une équipe de 9 personnes.</p>
+      </div>
+    </div>
+    <div class="card-experience">
+      <div class="info">
+        <h3 class="title">Référent technique national</h3>
+        <h4 class="card-experience-date">2014-2017</h4>
+        <h5 class="card-experience-location">ALD automotive - Nanterre (92)</h5>
+        <p class="card-experience-detail">Optimiser les coûts de réparations en carrosserie suite aux sinistres.</p>
+        <p class="card-experience-detail">Gérer les relations constructeurs pour l’obtention de participations financières.</p>
+      </div>
+    </div>
+    <div class="card-experience">
+      <div class="info">
+        <h3 class="title">Expert automobile</h3>
+        <h4 class="card-experience-date">2006-2014</h4>
+        <h5 class="card-experience-location">Cabinet Adam – Crécy la chapelle (77)</h5>
+        <p class="card-experience-detail">Réaliser les expertises terrain et à distance.</p>
+        <p class="card-experience-detail">Gérer des dossiers en Protection Juridique et Responsabilité Civile Professionnelle.</p>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ php/inc/templates/home.tpl.php
@@ -0,0 +1,40 @@
+<div class="banner">
+</div>
+<div class="card mb-3" style="max-width: 800px;">
+  <div class="row g-0 align-items-center">
+    <div class="col-md-5">
+      <img src="public/images/pierre-henri-kocan.jpeg" class="profil-picture" alt="profil-picture">
+    </div>
+    <div class="col-md-7">
+      <div class="card-body">
+        <h5 class="card-title">Pierre-Henri Kocan</h5>
+		<h6 class="card-function">Developpeur web - Back end</h6>
+		<div class="contacts">
+			<img src="public/images/phone.png" alt="phone-picture">
+			<p>+33(0)6.61.87.86.02</p>
+		</div>
+		<div class="contacts">
+			<img src="public/images/mail.png" alt="mail-picture">
+			<p>ph.kocan@icloud.com</p>
+		</div>
+		<div class="contacts">
+			<img src="public/images/home.png" alt="home-picture">
+			<p>Herblay-sur-Seine (95)</p>
+		</div>
+		<div class="contacts">
+			<img src="public/images/github.png" alt="github-picture">
+			<p>Pierre-Henri-Kocan</p>
+		</div>
+		<div class="contacts">
+			<img src="public/images/linkedin.png" alt="linkedin-picture">
+			<p>pierre-henri-kocan</p>
+		</div>
+      </div>
+    </div>
+  </div>
+</div>  
+
+<div class="about" style="max-width: 1000px;">
+	<h2>A propos de moi</h2>
+	<p>Après 15 ans passés dans le secteur de l’automobile et étant à la recherche d’un challenge professionnel, j’ai décidé de me former au métier de développeur web et web mobile. J’ai aujourd’hui acquis les compétences nécessaires pour débuter ce nouveau projet.</p>
+</div>
