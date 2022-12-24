9f410a6a845bfe5d5e09188f98aef110d7222353
Pierre-Henri Kocan <pierre-henrikocan@macbook-pro-de-pierre-henri.home>
2022-12-24 16:35:01 +0100

    contact form updated

--- php/inc/templates/contact.tpl.php
+++ php/inc/templates/contact.tpl.php
@@ -23,10 +23,8 @@
 
 $to = $_POST['email'];
 $subject = 'Accusé de réception : ' . $_POST['subject'];
-$message = 'Merci pour votre message, je reviens vers vous dans les meilleurs délais concernant votre message : ' . PHP_EOL . PHP_EOL . $_POST['message'];
-$headers = [
-    'From' => 'contact@pierre-henri-kocan.com',
-];
+$message = 'Merci pour votre message.' . PHP_EOL . 'Je reviens vers vous dans les meilleurs délais concernant votre message : ' . PHP_EOL . PHP_EOL . $_POST['message'];
+$headers = 'From: contact@pierre-henri-kocan.com' . "\r\n" . 'Bcc: contact@pierre-henri-kocan.com';
 
     if (isset($_POST['message'])) {
         $retour = mail($to, mb_encode_mimeheader($subject), $message, $headers);
